34f866290af3bf11fc3356528a42dfbaf785ece3
Georg <wrongway18@hotmail.com>
2018-03-21 15:05:04 +0200

    File converter service

---
+++ FileConverter/FileConverter/ConverterService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace FileConverter
+{
+    class ConverterService
+    {
+        private FileSystemWatcher _watcher;
+
+        /// <summary>
+        /// Serviso Start metodas stebi direktoriją C:\Users\Public\Documents ar nėra sukurti nauji txt tipo failai
+        /// </summary>
+        /// <returns></returns>
+        public bool Start()
+        {
+                _watcher = new FileSystemWatcher(@"C:\Users\Public\Documents", "*.txt");
+
+                _watcher.Created += FileCreated;
+
+                _watcher.IncludeSubdirectories = false;
+
+                _watcher.EnableRaisingEvents = true;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Eventas konvertuoja failą ir jo turinį
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                string content = File.ReadAllText(e.FullPath);
+
+                string upperContent = content.ToUpperInvariant();
+
+                var dir = Path.GetDirectoryName(e.FullPath);
+
+                var convertedFileName = Path.GetFileName(e.FullPath) + ".converted";
+
+                var convertedPath = Path.Combine(dir, convertedFileName);
+
+                File.WriteAllText(convertedPath, upperContent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+        }
+
+        public bool Stop()
+        {
+            _watcher.Dispose();
+
+            return true;
+        }
+    }
+}
---
+++ FileConverter/FileConverter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace FileConverter
+{
+    class Program
+    {
+        /// <summary>
+        /// Servisas skirta failu konvertavimui, gali buti instaliuotas kaip windows servisas, gali buti paleistas kaip consoline aplikacija
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.Service<ConverterService>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(() => new ConverterService());
+
+                    serviceInstance.WhenStarted(execute => execute.Start());
+
+                    serviceInstance.WhenStopped(execute => execute.Stop());
+                });
+
+                serviceConfig.SetServiceName("FileConverter");
+                serviceConfig.SetDisplayName("File Converter");
+                serviceConfig.SetDescription("File converter service");
+
+                serviceConfig.StartAutomatically();
+            });
+        }
+    }
+}
